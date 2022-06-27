61c48f614b91b1ce67031450d84cd3c5e929e7d7
Andy Britt <AndyB.WebDev@protonmail.com>
2022-06-27 10:27:07 -0400

    ejs working and new pages

--- index.js
+++ index.js
@@ -7,51 +7,47 @@
 ; Reference: https://www.geeksforgeeks.org/how-to-setup-view-engine-in-node-js/
 ; Date referenced: 19 Jun 2022
 */
-
-// Imports
 const express = require('express');
-const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Static files
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static('views'));
-//app.use('/images', express.static(__dirname + 'public/images'));
-//app.use('/styles', express.static(__dirname + 'public/styles'));
-//app.use('/js', express.static(__dirname + 'public/js'));
+app.use(express.static("public"));
+//app.use('/images', express.static(__dirname + "public/images"));
+//app.use('/styles', express.static(__dirname + "public/styles"));
+//app.use('/js', express.static(__dirname + "public/js"));
 
-// Adding EJS to the apps view engine
+// Adding and setting EJS to the apps view engine
 app.engine('.html', require('ejs').__express);
-
-// Setting views to HTML files directory
-app.set('views', path.join(__dirname, 'views'));
-
-// Setting view engine to HTML
-app.set('view engine', 'html');
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 // Sending Index HTML
-app.get('/', (req, res)=> 
+app.get('', (req, res)=>
 {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+    res.render('index.html');
 });
 
 // Sending Grooming page
 app.get('/grooming', (req, res)=> 
 {
-    res.sendFile(path.join(__dirname + '/views/grooming.html'));
+    res.render('grooming.html');
 });
 
-// Hello, is it me you're looking for? 404
-app.get('/', (req, res)=> 
+// Sending Training page
+app.get('/training', (req, res)=> 
 {
-    res.sendFile(path.join(__dirname + '/views/grooming.html'));
+    res.render('training.html');
 });
 
-// Listen on port 3000
+// Sending Boarding page
+app.get('/boarding', (req, res)=> 
+{
+    res.render('boarding.html');
+});
+
+// Listen and Logging on port 3000
 app.listen(PORT, ()=>
 {
-    console.log('This application is listening on ' + PORT);
+    console.log('The application is listening on ' + PORT);
 });
-
