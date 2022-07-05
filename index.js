0c74166c812a9a2a16e33b7d924e2366c3c3a287
Andy Britt <AndyB.WebDev@protonmail.com>
2022-07-05 13:30:23 -0400

    adding more backend stuff

--- index.js
+++ index.js
@@ -10,8 +10,27 @@
 
 //requiring Express and declaring the port used
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session');
+const moment = require('moment');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// variable for database
+const CONN = "db stuff goes here"
+
+// connecting to the database
+mongoose.connect(CONN).then(() => {
+    console.log('Connection to the database was successful\n   You are not a failure!');
+}).catch(err => {
+    console.log('MongoDB Error: ' + err.message);
+})
+
+// mongoose model imports
+const User = require('./models/user.js');
 
 // Static files
 app.use(express.static("public"));
@@ -20,12 +39,33 @@ app.use('/images', express.static(__dirname + 'public/images'));
 app.use('/styles', express.static(__dirname + 'public/styles'));
 app.use('/js', express.static(__dirname + 'public/js'));
 
+// Week 6 addition - 
+// need to accept forms
+app.use(express.urlencoded({ extended: false })); 
+// needed to parse delicious cookies
+app.use(cookieParser());
+// needed to accept and return JSON objects
+app.use(express.json());
+// Express session ?secret?
+app.use(session({
+    secret: 's3cret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Adding and setting EJS to the apps view engine
 app.engine('.html', require('ejs').__express);
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-// Sending Index HTML
+// Sending Index HTML 
 app.get('', (req, res)=>
 {
     res.render('index.html');
@@ -49,8 +89,55 @@ app.get('/boarding', (req, res)=>
     res.render('boarding.html');
 });
 
+// Getting Registration page
+app.get('/register', (req, res) => {
+    User.find({}, function(err, users) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('register.html', {
+                title: 'FMS: Register',
+                message: message,
+                cardTitle: 'Registration Form',
+                moment: moment,
+                users: users
+            })
+        }
+    })
+})
+
+// Sending Registration Data
+app.post('/register', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.register(new User({username: username}), password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.redirect('/register');
+        }
+
+        passport.authenticate("local")(
+            req, res, function () {
+                res.redirect('/register')
+            });
+    })
+})
+
+// Sending Login page
+app.get('/login', (req, res)=> 
+{
+    res.render('login.html');
+});
+
+// Getting Login Data
+app.post('/login', (req, res) => {
+    req.body.email
+});
+
 // Listen and Logging on port 3000
 app.listen(PORT, ()=>
 {
     console.log('The application is listening on ' + PORT);
 });
+
